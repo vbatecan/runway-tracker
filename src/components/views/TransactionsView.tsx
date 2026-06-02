7fe49d9e0f02b916e90e723d8536804a1e73aa6e
Debian <nytri@Personal>
2026-06-02 11:02:28 +0800

    fix: remove unused formatCurrency import to fix build

--- src/components/views/TransactionsView.tsx
+++ src/components/views/TransactionsView.tsx
@@ -1,5 +1,4 @@
 import { ExpenseItem, IncomeItem, CashPosition, SupportedCurrency, AppData } from '@/types';
-import { formatCurrency } from '@/utils/currency';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { TransactionList } from '@/components/TransactionList';
 import { CashInput } from '@/components/CashInput';
